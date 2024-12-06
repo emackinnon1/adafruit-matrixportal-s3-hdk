75183956e87542df527136a1d881414f90d09d38
emackinnon1 <emackinnon100390@gmail.com>
2024-12-05 19:39:55 -0700

    Remove secrets

--- secrets.h
+++
@@ -1,9 +0,0 @@
-#define TIDBYT_WIFI_SSID "Who pooped in the pool"
-#define TIDBYT_WIFI_PASSWORD "Sonoffinan1!"
-// #define TIDBYT_REMOTE_URL "https://axilla.netlify.app/?applet=https://raw.githubusercontent.com/tidbyt/community/main/apps/eplscores/eplscores.star&selectedTeam=ARS&rotationSpeed=5&displayType=colors&pregameDisplay=record&displayTop=league&displayTimeColor=%23FFA500&format=webp&output=image"
-
-// https://axilla.netlify.app/?applet=https://raw.githubusercontent.com/tidbyt/community/main/apps/digitalrain/digital_rain.star&color=green&char_size=normal&format=webp&output=image
-// https://axilla.netlify.app/?applet=https://raw.githubusercontent.com/tidbyt/community/main/apps/martamap/marta_map.star&station=Five+Points&arrivals=false&scroll=true&text_color=%23aaaaaa&orientation=false&format=webp&output=image
-// https://axilla.netlify.app/?applet=https://raw.githubusercontent.com/tidbyt/community/main/apps/eplscores/eplscores.star&selectedTeam=ARS&rotationSpeed=5&displayType=colors&pregameDisplay=record&displayTop=league&displayTimeColor=%23FFA500&format=webp&output=image
-// https://axilla.netlify.app/?applet=https://raw.githubusercontent.com/tidbyt/community/main/apps/mvv/mvv.star&stop=%7B%22display%22%3A%22Hauptbahnhof%22%2C%22text%22%3A%22Hauptbahnhof%22%2C%22value%22%3A%22de%3A09162%3A6%22%7D&format=webp&output=image
-// https://axilla.netlify.app/?applet=https://raw.githubusercontent.com/tidbyt/community/main/apps/eplscores/eplscores.star&selectedTeam=ARS&rotationSpeed=5&displayType=colors&pregameDisplay=record&displayTop=league&displayTimeColor=%23FFA500&format=webp&output=image
